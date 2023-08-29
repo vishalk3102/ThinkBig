a117ee437ab0a1c3749a8c669a77f70ca86da0ce
vishal kumar <vishal.k3102@gmail.com>
2023-08-30 03:41:50 +0530

    course and user  code added

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import app from './app.js'
 import connectDB from './config/database.js'
 import cloudinary from 'cloudinary'
 // import Razorpay from 'razorpay'
-// import nodeCron from 'node-cron'
-// import { Stats } from './models/StatsModel.js'
+import nodeCron from 'node-cron'
+import { Stats } from './models/StatsModel.js'
 
 connectDB()
 
@@ -18,13 +18,13 @@ cloudinary.v2.config({
 //   key_secret: process.env.RAZORPAY_API_SECRET,
 // });
 
-// nodeCron.schedule("0 0 0 1 * *", async () => {
-//   try {
-//     await Stats.create({});
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
+nodeCron.schedule('0 0 0 1 * *', async () => {
+  try {
+    await Stats.create({})
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on port: ${process.env.PORT}`)
